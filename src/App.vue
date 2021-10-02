728f00f82af830ce35b580a0fe31a37c8e14fcfd
Picsong <312465087@qq.com>
2021-10-02 10:33:09 +0800

    chore(global): 添加eslint规范代码

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="wrap">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>9</div>
+    <div>9</div>
+    <div>9</div>
+    <div>9</div>
+    <div>9</div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.wrap{
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  width: 500px;
+  height: 500px;
+  border: 1px solid #333;
+}
+.wrap>div{
+  width: 60px;
+  height: 80px;
+  border: 1px dashed #304455;
+  background-color: #42b983;
+}
+</style>
