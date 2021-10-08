c81ac826f84f725d5936ee1838368de91fbd8965
Picsong <312465087@qq.com>
2021-10-08 10:32:06 +0800

    feat(commit规范): 使用commitlint校验，添加commit-msg钩子

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueJsx from "@vitejs/plugin-vue-jsx"
 
 export default defineConfig({
-  plugins: [vue({
-    template: {
-      compilerOptions: {}
-    }
-  }), vueJsx()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {},
+      },
+    }),
+    vueJsx(),
+  ],
   resolve: {
     alias: {
-      vue: 'vue/dist/vue.esm-bundler.js',
-      '@components': '/src/components',
-      '@hooks': '/src/hooks',
-      '@': '/src'
-    }
-  }
+      vue: "vue/dist/vue.esm-bundler.js",
+      "@": "/src",
+      "@hooks": "/src/hooks",
+      "@components": "/src/components",
+    },
+  },
 })
